cf8eae87b883c064027498e720987e7ae0bbef69
Venicck <k9nine1205@gmail.com>
2025-06-09 18:34:15 +0900

    サイト 大幅更新

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
+site_project = 'https://api.github.com/repos/Venicck/Introduction';
 primarybody = document.getElementById('body-primary');
 secondarybody = document.getElementById('body-secondary');
 topname = document.getElementById('topbar-name');
+lastupdated = document.getElementById('last-updated');
 
 function ShowPrimary() {
+    let favicon = document.querySelector("link[rel~='icon']");
     secondarybody.style.display = 'none';
     primarybody.style.display = 'block';
-    topname.textContent = "ベニック の ホームページ";
+    document.title = 'ベニックのホームページ';
+    favicon.href = './img/veni.png';
 }
 
 function ShowSecondary() {
+    let favicon = document.querySelector("link[rel~='icon']");
     primarybody.style.display = 'none';
     secondarybody.style.display = 'block';
-    topname.textContent = "ぽりとす の ホームページ";
+    document.title = 'ぽりとすのホームページ'; // サイトタイトルを変更
+    favicon.href = './img/poly.png'
 }
 
 function GoTo(link) {
     window.open(link, '_blank');
+}
+
+window.onload = function() {
+    let pram_urlstr = window.location.href;
+    let pram_url = new URL(pram_urlstr);
+    let params = pram_url.searchParams.get('t');
+    if (params == null) {
+        ShowPrimary();
+    } else if (params == 'v') {
+        ShowPrimary();
+    } else if (params == 'p') {
+        ShowSecondary();
+    }
+
+    fetch(site_project).then(
+        data => data.json()
+    ).then (
+        json => {
+            lastupdated.textContent = 'Last Updated: ' + json.updated_at.split('T')[0];
+        }
+    );
+
 }
